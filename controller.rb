03776d0929165da7fced5b8e7e38d8fef979017c
chirs <chrisedgemon@gmail.com>
2011-07-26 20:32:14 -0400

    Simpler controller to initialize Sinatra app, add Gemfile

--- controller.rb
+++ controller.rb
@@ -1,31 +1,5 @@
-require 'rubygems'
 require 'sinatra'
-require 'datamapper'
-
-
-DataMapper::Database.setup({
-  :adapter  => 'sqlite3',
-  :host     => 'localhost',
-  :username => '',
-  :password => '',
-  :database => 'db/restaurant_development'
-})
 
 get '/' do
-    'Hello from Sinatra on Heroku'
-end
-
-get '/restaurants' do
-  @restaurants = Restaurant.all
-  haml view :restaurants
-end
-
-get '/restaurant/detail/:id' do
-  @restaurant = Restaurant.find :id => params[:id]
-  haml view :restaurant_detail
-end
-
-get '/restaurant/reserve/:table_id' do
-  @table = Table.find :id => params[:table_id]
-  haml view :reserve
+  "Hello World!"
 end
