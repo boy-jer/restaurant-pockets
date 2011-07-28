216cac6b21c52c91b38cc842b103858b0ad68ab0
chirs <chrisedgemon@gmail.com>
2011-07-28 01:41:07 -0400

    Remove mongo logic from controller

--- controller.rb
+++ controller.rb
@@ -14,14 +14,4 @@ end
 
 post '/add' do
   name = params[:name]
-  db = Mongo::Connection.new("staff.mongohq.com/restaurant", 10016).db("database")
-  auth = db.authenticate("chris", "password")
-  coll = db.collection("restaurants")
-  doc = {"name" => name}
-  coll.insert(doc)
-  if request.xhr?
-    "true"
-  else
-    "We've added your restaurant"
-  end
 end
