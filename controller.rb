a505ee52bb3fca3f284c9efba91dc6190d8967f8
chirs <chrisedgemon@gmail.com>
2011-07-28 20:50:27 -0400

    Nothing much

--- controller.rb
+++ controller.rb
@@ -6,14 +6,9 @@ set :haml, :format => :html5
 
 get '/' do  
   @this = "Can you do it?"
-  @lst = [41...47]
+  @lst = (41...47)
   haml :index
 end
-
-get '/hello/:name' do |n|
-    "Hello #{n}"
-end
-
 
 post '/add' do
   name = params[:name]
